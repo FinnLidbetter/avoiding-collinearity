07852e8f8cd3c61d4e97d860f298b7395df9b3fd
Finn Lidbetter <finnlidbetter@gmail.com>
2021-12-29 18:53:49 -0500

    Add independent command to count collinear traps

---
+++ src/CountCollinearTrapezoids.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+
+public class CountCollinearTrapezoids {
+
+    public static void main(String[] args) {
+        if (isHelpArgument(args[0])) {
+            printHelp();
+            System.exit(0);
+        }
+        if (args.length != 2) {
+            printHelp();
+            System.exit(1);
+        }
+        try {
+            int maxIndexGap = Integer.parseInt(args[0]);
+            String numberSystem = args[1];
+            if (numberSystem.equals("wholeAndRt3")) {
+                Point<WholeAndRt3> zeroPoint = new Point<>(WholeAndRt3.ZERO, WholeAndRt3.ZERO);
+                TrapezoidSequence<WholeAndRt3> trapSeq = new TrapezoidSequence<>(2, zeroPoint);
+                int lastNewSubwordIndex = trapSeq.indexOfLastNewSubword(maxIndexGap, null);
+                Interval[] searchIntervals = trapSeq.getCollinearSearchIntervals(
+                        maxIndexGap, lastNewSubwordIndex + maxIndexGap
+                );
+                System.out.printf("Intervals to search: %s.\n", Arrays.toString(searchIntervals));
+                int maxCollinearCount = 0;
+                for (Interval searchInterval: searchIntervals) {
+                    int intervalMaxCollinearCount = trapSeq.radialSweepCountCollinear(
+                            searchInterval.lo, searchInterval.hi, maxIndexGap
+                    );
+                    if (intervalMaxCollinearCount > maxCollinearCount) {
+                        maxCollinearCount = intervalMaxCollinearCount;
+                    }
+                }
+                System.out.printf(
+                        "The largest number of trapezoids separated by at most %d indices that\n" +
+                        "are intersected by a single straight line is %d.\n", maxIndexGap, maxCollinearCount
+                );
+            } else if (numberSystem.equals("wholeNumber")) {
+                Point<WholeNumber> zeroPoint = new Point<>(WholeNumber.ZERO, WholeNumber.ZERO);
+                TrapezoidSequence<WholeNumber> trapSeq = new TrapezoidSequence<>(2, zeroPoint);
+                int lastNewSubwordIndex = trapSeq.indexOfLastNewSubword(maxIndexGap, null);
+                Interval[] searchIntervals = trapSeq.getCollinearSearchIntervals(
+                        maxIndexGap, lastNewSubwordIndex + maxIndexGap
+                );
+                System.out.printf("Intervals to search: %s.\n", Arrays.toString(searchIntervals));
+                int maxCollinearCount = 0;
+                for (Interval searchInterval: searchIntervals) {
+                    int intervalMaxCollinearCount = trapSeq.radialSweepCountCollinear(
+                            searchInterval.lo, searchInterval.hi, maxIndexGap
+                    );
+                    if (intervalMaxCollinearCount > maxCollinearCount) {
+                        maxCollinearCount = intervalMaxCollinearCount;
+                    }
+                }
+                System.out.printf(
+                        "The largest number of trapezoids separated by at most %d indices that\n" +
+                        "are intersected by a single straight line is %d.\n", maxIndexGap, maxCollinearCount
+                );
+            } else {
+                printHelp();
+                System.exit(1);
+            }
+        } catch (NumberFormatException e) {
+            printHelp();
+            System.exit(1);
+        }
+
+    }
+    private static void printHelp() {
+        System.out.println("Usage:");
+        System.out.println("\tjava CountCollinearTrapezoids maxIndexGap \"wholeAndRt3\"|\"wholeNumber\"");
+        System.out.println("Examples:");
+        System.out.println("java CountCollinearTrapezoids 7 wholeAndRt3");
+        System.out.println("java CountCollinearTrapezoids 7 wholeNumber");
+    }
+
+    private static boolean isHelpArgument(String str) {
+        return str.contains("help") || str.equals("-h");
+    }
+}
