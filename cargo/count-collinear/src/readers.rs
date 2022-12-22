347a9a2c36d3f2b4cef8d39ef9511cd47357fd3f
Finn Lidbetter <finnlidbetter@gmail.com>
2022-12-22 15:57:44 -0500

    Add tests for parse CountCollinearArgs from_str

--- cargo/count-collinear/src/readers.rs
+++ cargo/count-collinear/src/readers.rs
@@ -73,6 +73,7 @@ pub fn parse_args_from_strings(
     }))
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct CountCollinearArgs {
     pub(crate) sequence_length: u32,
     pub(crate) start_index: usize,
@@ -93,7 +94,7 @@ impl fmt::Display for CountCollinearArgs {
 ///
 /// This assumes that base_str is ascii and that only the digits
 /// should be parsed. As such, this only works for nonnegative integers.
-/// Parsing will stop when a comma ',' is reached.
+/// Parsing will stop when a comma ',' or right curly brace '}' is reached.
 fn parse_digits_after_pattern<T: FromStr>(
     text: &str,
     pattern: &str,
@@ -104,7 +105,7 @@ fn parse_digits_after_pattern<T: FromStr>(
         Some(sequence_length_start) => {
             let mut sequence_length_digits = String::new();
             let mut char_index = sequence_length_start;
-            while text_chars[char_index] as char != ',' {
+            while text_chars[char_index] != ',' && text_chars[char_index] != '}' {
                 if text_chars[char_index].is_digit(10) {
                     sequence_length_digits.push(text_chars[char_index]);
                 }
@@ -172,4 +173,72 @@ where
     fn is_finished_reading(&self) -> bool;
 
     fn stop_reading(&self) -> ();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_collinear_args_from_str() {
+        let good_string_1 = "{\"sequence_length\": 1000, \"start_index\": 50, \"end_index\": 60}";
+        let good_string_2 = "{\"sequence_length\": 1000, \"end_index\": 60, \"start_index\": 50}";
+        let good_string_3 = "{\"start_index\": 50, \"sequence_length\": 1000, \"end_index\": 60}";
+        let good_string_4 = "{\"start_index\": 50, \"end_index\": 60, \"sequence_length\": 1000}";
+        let good_string_5 = "{\"end_index\": 60, \"start_index\": 50, \"sequence_length\": 1000}";
+        let good_string_6 = "{\"end_index\": 60, \"sequence_length\": 1000, \"start_index\": 50}";
+        let expected_args = CountCollinearArgs {
+            sequence_length: 1000,
+            start_index: 50,
+            end_index: 60,
+        };
+        assert_eq!(
+            CountCollinearArgs::from_str(good_string_1),
+            Ok(expected_args.clone())
+        );
+        assert_eq!(
+            CountCollinearArgs::from_str(good_string_2),
+            Ok(expected_args.clone())
+        );
+        assert_eq!(
+            CountCollinearArgs::from_str(good_string_3),
+            Ok(expected_args.clone())
+        );
+        assert_eq!(
+            CountCollinearArgs::from_str(good_string_4),
+            Ok(expected_args.clone())
+        );
+        assert_eq!(
+            CountCollinearArgs::from_str(good_string_5),
+            Ok(expected_args.clone())
+        );
+        assert_eq!(
+            CountCollinearArgs::from_str(good_string_6),
+            Ok(expected_args.clone())
+        );
+        let extra_whitespace =
+            "{\"sequence_length\": 1000,\n \"start_index\":   50,             \"end_index\": 60}";
+        assert_eq!(
+            CountCollinearArgs::from_str(extra_whitespace),
+            Ok(expected_args.clone())
+        );
+        let missing_seqeunce_length = "{\"end_index\": 60, \"start_index\": 50}";
+        assert_eq!(
+            CountCollinearArgs::from_str(missing_seqeunce_length),
+            Err(ParseCountCollinearArgsErr {
+                msg: format!(
+                    "{} not found in {} when trying to parse CountCollinearArgs.",
+                    "sequence_length", missing_seqeunce_length
+                )
+            })
+        );
+        // The sequence length value is missing.
+        let malformed_string = "{\"sequence_length\": , \"start_index\": 100, \"end_index\": 124}";
+        assert_eq!(
+            CountCollinearArgs::from_str(malformed_string),
+            Err(ParseCountCollinearArgsErr {
+                msg: String::from("Could not parse ")
+            })
+        );
+    }
 }
